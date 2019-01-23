39cdd658a97c4c1276dd5880c604eb959be04c9e
MrJ.W <komercyjnefy@gmail.com>
2019-01-23 03:02:14 +0100

    Poprawki w logowaniu z webowej aplikacji

--- api/site/res/login.js
+++ api/site/res/login.js
@@ -1,23 +1,30 @@
+var loginPanelhtml;
+$(document).ready(()=>{
+    loginPanelhtml = $("#main").html();
+});
+
 function login()
 {
+    $("#alert").remove();
     let user = new User();
     user.email = $("#email").val();
     user.getToken($("#password").val(), loginSucess, loginError);
+    Responsive.placeSpinner("main", "Signing in");
 }
 
 function loginSucess(data)
 {
-    let message = "<div id='alert' class='alert alert-success col-md-5 align-self-center' role='alert'>Signing in</div>";
-    $("#alert").remove();
-    $('#allerts').after(message);
+    // let message = "<div id='alert' class='alert alert-success col-md-5 align-self-center' role='alert'>Signing in</div>";
+    // $("#alert").remove();
+    // $('#allerts').after(message);
     Core.setCookie("token", data["body"], 1);
-    checkLoggedInterval = window.setInterval(checkLogged, 10000);
-    //Core.sendToHome();
+    Core.sendToHome();
 }
 
 function loginError(data)
 {
-    console.log(data);
+    $("#main").html(loginPanelhtml);
+    //console.log(data);
     let message = "<div id='alert' class='alert alert-danger col-md-5 mt-3 align-self-center' role='alert'>Login or password incorrect.</div>";
     $("#alert").remove();
     $('#allerts').after(message);
@@ -48,6 +55,7 @@ function checkLoggedError(data)
 {
     console.log("checkLoggedError");
     console.log(data);
+    //$("#main").html(loginPanelhtml);
     Core.deleteCookie("token");
     if(Core.getCurrentFile() == homeFile)
     {
